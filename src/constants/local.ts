088080d413acb2fb2c983b46f8e15fd7bef3e433
JamesDong <james@vinova.com.sg>
2025-12-19 21:19:57 +0700

    chore: remove the token key

--- src/constants/local.ts
+++ src/constants/local.ts
@@ -1,6 +1,5 @@
 //TODO: Pase your token here for testing purpose
-export const TOKEN_KEY =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTg0NTQ4NGRmMzRkOWExODAzZjYwMTAzZjg3MzhmYyIsIm5iZiI6MTY5MjQxMjg4OC40MDgsInN1YiI6IjY0ZTAyYmQ4ZTE5ZGU5MDExZDVlMTYzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OEa-H-0hyxQ4PGR5aALnh11HeI38pUuD50TTTc3PPYQ";
+export const TOKEN_KEY = "";
 
 export const KEY_ID_STORE = "aquariux_store_v1";
 
